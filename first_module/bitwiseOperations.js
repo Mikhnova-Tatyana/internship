40281247106bf4b6946751392d2fcb3481bc0029
Tanya <tanjamikhnva@gmail.com>
2022-02-10 10:19:12 +0200

    add errrors processing

--- first_module/bitwiseOperations.js
+++ first_module/bitwiseOperations.js
@@ -42,6 +42,9 @@ function bitwiseInvert(number) {
 }
 
 function bitwiseInvert(number) {
+    if (typeof number !== 'number') {
+        throw new Error('Invalid argument');
+    }
     for (let i = 0; i < 32; i++){
         if (((number >> i) & 1) === 0) {
             number ^= (0 << i);
