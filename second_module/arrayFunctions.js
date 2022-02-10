128fc241194a18068d95d4ee7b31b30ba2405ec8
TaMi-coder <68444923+TaMi-coder@users.noreply.github.com>
2022-02-11 00:01:39 +0200

    add array_functions

--- second_module/arrayFunctions.js
+++ second_module/arrayFunctions.js
@@ -1,12 +1,61 @@
-Array.prototype.customForEach = function() {
-
+Array.prototype.customForEach = function (callback) {
+    if (typeof callback !== 'function') {
+        throw new Error('Callback is not a function');
+    }
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i], i, this);
+    }
 }
 
-function customForEach(array, callback) {
-    for (let i = 0; i < array.length; i++) {
-        callback(array[i]) {
-            
+Array.prototype.customMap = function (callback) {
+    if (typeof callback !== 'function') {
+        throw new Error('Callback is not a function');
+    }
+    const resultArray = [];
+    for (let i = 0; i < this.length; i++) {
+        resultArray.push(callback(this[i], i, this));
+    }
+    return resultArray;
+}
+
+Array.prototype.customFilter = function (callback) {
+    if (typeof callback !== 'function') {
+        throw new Error('Callback is not a function');
+    }
+    const resultArray = [];
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            resultArray.push(this[i]);
         }
     }
-} 
-    
+    return resultArray;
+}
+
+Array.prototype.customFind = function (callback) {
+    if (typeof callback !== 'function') {
+        throw new Error('Callback is not a function');
+    }
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            return this[i];
+        }
+    }
+    return undefined;
+}
+
+Array.prototype.customReduce = function (callback, accumulator) {
+    if (typeof callback !== 'function') {
+        throw new Error('Callback is not a function');
+    }
+    let index = 0;
+    if (arguments.length < 2) {
+        index = 1;
+        accumulator = this[0];
+    }
+
+    while (index < this.length) {
+        accumulator = callback(accumulator, this[index], index, this);
+        ++index;
+    }
+    return accumulator;
+}
