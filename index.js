d6e780821f4fcd8f25c5c5dfb35a2a22c146b3ba
Tanya <tanjamikhnva@gmail.com>
2022-02-14 17:49:18 +0200

    add binary tree

---
+++ index.js
@@ -0,0 +1,328 @@
+// Рекурсия
+
+function min(arr, index) {
+    index = index || 0;
+
+    let result = arr[index]; // 2
+    let value = null;
+
+    if (arr.length > index) {
+        value = min(arr, index + 1); // 1
+    }
+
+    if (value !== null && result > value) { // 2 > 3
+        return value;
+    }
+
+    return result;
+}
+
+min([1, 2, 3, 4])
+
+
+function f(arr, index) {
+    index = index || 0;
+
+    if (arr.length <= index) {
+        return 0;
+    }
+
+    return arr[index] + f(arr, ++index);
+}
+
+console.dir(f([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+
+function f(arr, index) {
+    index = index || 0;
+
+    if (arr.length > index) {
+        f(arr, index + 1);
+        console.log(arr[index]);
+    }
+}
+
+f([1, 2]);
+
+
+const factorialRecursionMemo = (function () {
+    let memo = {};
+    return function factorial(number) {
+        if (number === 0) {
+            return 1;
+        }
+
+        if (memo[number] === undefined) {
+            memo[number] = factorial(number - 1);
+        }
+
+        return number * memo[number];
+    };
+})();
+
+factorialRecursionMemo(5)
+
+
+
+
+// Генераторы и итераторы
+
+
+function* f(max, index) {
+    index = index || 0;
+
+    if (index++ < max) {
+        yield* f(max, index);
+    }
+
+    yield index;
+}
+
+for (let item of f(5)) {
+    console.log(item);
+}
+
+
+let obj = {
+    min: 0,
+    max: 10,
+    [Symbol.iterator]: function* () {
+        for (let i = this.min; i < this.max; i++) {
+            yield i * 2;
+        }
+    },
+};
+
+
+/*
+let result = obj.customMap(word => {
+    return {
+        word,
+        lehgth: word.length,
+    };
+});
+console.log(result);*/
+
+
+function* f() {
+    for (let i = 0; i < 3; i++) {
+        yield* f2();
+    }
+
+    return 7;
+}
+
+function* f2() {
+    for (let i = 0; i < 3; i++) {
+        yield i * 2;
+    }
+}
+
+for (let item of f()) {
+    console.log(item);
+}
+
+
+let obj = {
+    min: 0,
+    max: 10,
+    [Symbol.iterator]() {
+        return {
+            current: this.min,
+            max: this.max,
+            next() {
+                if (this.current < this.max) {
+                    return {
+                        value: this.current++,
+                        done: false,
+                    };
+                } else {
+                    return {
+                        value: undefined,
+                        done: true,
+                    };
+                }
+            },
+        };
+    },
+};
+
+Object.prototype.customMap = function (callback) {
+    if (this[Symbol.iterator] === undefined) {
+        throw new Error("Object isn't iterable");
+    }
+
+    let result = [];
+
+    let iterator = this[Symbol.iterator]();
+    let next = iterator.next();
+    for (; !next.done;) {
+        result.push(callback(next.value));
+        next = iterator.next();
+    }
+
+    return result;
+};
+let obj2 = {};
+let result = [4, 5, 6].customMap(value => value * 2);
+console.dir(result);
+/*
+for(let item of obj) {
+    console.log(item);
+}*/
+
+
+let obj = {
+    str: "asd dfuhg ier sdfguhdf js bgdsjfhdgf hf fh",
+    [Symbol.iterator]() {
+        return {
+            index: 0,
+            str: this.str.split(" "),
+            next() {
+                if (this.index < this.str.length) {
+                    return {
+                        value: this.str[this.index++],
+                        done: false,
+                    };
+                }
+
+                return {
+                    value: undefined,
+                    done: true,
+                };
+            },
+        };
+    },
+};
+
+
+
+let result = obj.customMap(word => {
+    return {
+        word,
+        lehgth: word.length,
+    };
+});
+console.log(result);
+
+
+function* f() {
+    for (let i = 0; i < 3; i++) {
+        yield* f2();
+    }
+
+    return 7;
+}
+
+function* f2() {
+    for (let i = 0; i < 3; i++) {
+        yield i * 2;
+    }
+}
+
+for (let item of f()) {
+    console.log(item);
+}
+
+
+// Кастомный bind
+
+function myBind(func, context) {
+    return function () {
+        let symbol = Symbol();
+        context[symbol] = func;
+        let result = context[symbol]();
+        delete context[symbol];
+        return result;
+    };
+}
+
+function f() {
+    return this.a + 1;
+}
+
+let obj = {
+    a: 10,
+};
+
+let f2 = myBind(f, obj);
+console.dir(f2());
+
+console.log(Symbol() === Symbol());
+
+
+function myBind(func, context) {
+    let self = Object.create(context);
+    return function () {
+        self.func = func;
+        return self.func();
+    };
+}
+
+// Образец
+
+function getSumAllNumber(array, index) { 
+    index = index || 0; 
+    let sum = array[index]; 
+    if (++index < array.length) { 
+        sum += getSumAllNumber(array, index); 
+    } 
+    return sum;
+}
+getSumAllNumber([1, 2, 3, 4])
+
+
+function getSumOddNumber(arrayOfNumbers, index) { 
+    index = index || 0; 
+    let sum = 0; 
+    if (arrayOfNumbers[index] % 2 !== 0) { 
+        sum += arrayOfNumbers[index]; 
+    } 
+    if (++index < arrayOfNumbers.length) {
+        sum += getSumOddNumber(arrayOfNumbers, index); 
+    }
+    return sum;
+}
+
+getSumOddNumber([1, 2, 3, 4])
+
+// customBind
+
+function myBind(func, context) {
+  return function () {
+    let symbol = Symbol();
+    context[symbol] = func;
+    let result = context[symbol]();
+    delete context[symbol];
+    return result;
+};
+}
+
+function f() {
+return this.a + 1;
+}
+
+let obj = {
+a: 10,
+};
+
+let f2 = myBind(f, obj);
+console.dir(f2());
+
+console.log(Symbol() === Symbol())
+
+function f() {
+let symbol = Symbol();
+let symbol2 = Symbol();
+return {
+    [symbol]: 2,
+    [symbol2]: 22,
+    a: 6,
+};
+}
+
+let obj = f();
+let symbols = Object.getOwnPropertySymbols(obj);
+
+console.log(obj[symbols[0]]);
+
---
+++ third_module/binaryTree.js
@@ -0,0 +1,202 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.leftChild = null;
+    this.rightChild = null;
+    this.parent = null;
+  }
+}
+
+class BinTree {
+  constructor() {
+    this.root = null;
+  }
+  
+  insert(newNode) {
+    // Если корень дерева пуст
+    if (this.root === null) {
+      this.root = newNode;
+      return;
+    }
+    
+    function _insert(currentNode) {
+      if (newNode.data < currentNode.data) {
+        if (currentNode.leftChild !== null) {
+          _insert(currentNode.leftChild);
+        } else {
+          newNode.parent = currentNode;
+          currentNode.leftChild = newNode;
+        }
+      } else {
+        if (currentNode.rightChild !== null) {
+          _insert(currentNode.rightChild);
+        } else {
+          newNode.parent = currentNode;
+          currentNode.rightChild = newNode;
+        }
+      }
+    }
+    // Ищем куда вставить, стартуем с корня
+    _insert(this.root);
+  }
+  
+  search(data) {
+    
+    function _search(currentNode) {
+      if (currentNode === null)
+        return null;
+      if (currentNode.data === data)
+        return currentNode;
+      if (currentNode.data > data)
+        return _search(currentNode.leftChild);
+      else
+        return _search(currentNode.rightChild);
+    }
+    return _search(this.root);
+  }
+  
+  delete(data) {
+    // Для удаления элемента с одним, или нулем, наследников
+    function deleteOneOrNoChildElem(node) {
+      let newNode = null;
+      if (node.leftChild)
+        newNode = node.leftChild;
+      else if (node.rightChild)
+        newNode = node.rightChild;
+      
+      if (node.parent === null) {
+        this.root = newNode;
+      } else {
+        if (node.parent.leftChild === node) {
+          node.parent.leftChild = newNode;
+        } else {
+          node.parent.rightChild = newNode;
+        }
+      }
+    }
+    // Для удаления элемента с двумя наследниками
+    function deleteTwoChildElem(node) {
+      let result = node.leftChild;
+      if (result.rightChild) {
+        while (result.rightChild) {
+          result = result.rightChild;
+        }
+      }
+      deleteOneOrNoChildElem(result);
+      let leftNodeChild = node.leftChild;
+      let rightNodeChild = node.rightChild;
+      
+      if (node.parent === null) {
+        result.leftChild = leftNodeChild;
+        result.rightChild = rightNodeChild;
+        this.root = result;
+      } else {
+        if (node.parent.leftChild === node) {
+          result.leftChild = leftNodeChild;
+          result.rightChild = rightNodeChild;
+          node.parent.leftChild = result;
+        } else {
+          result.leftChild = leftNodeChild;
+          result.rightChild = rightNodeChild;
+          node.parent.rightChild = result;
+        }
+      }
+    }
+    
+    let node = this.search(data);
+    if (node.leftChild && node.rightChild) {
+      deleteTwoChildElem.call(this, node);
+    }
+    else {
+      deleteOneOrNoChildElem.call(this, node);
+    }
+  
+  }
+}
+
+let binTree = new BinTree();
+binTree.insert(new Node(10))
+binTree.insert(new Node(20))
+binTree.insert(new Node(-10))
+binTree.insert(new Node(30))
+binTree.insert(new Node(41))
+binTree.insert(new Node(25))
+console.log(binTree);
+console.log(binTree.search(41));
+binTree.delete(20)
+
+
+
+class Node {
+  constructor(data) {
+      this.data = data;
+      this.left = null;
+      this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+      this.root = null;
+  }
+
+  insert(data) {
+    let newNode = new Node(data);
+      if (this.root === null) {
+        this.root = newNode;
+        return;
+      } 
+      let currentNode = this.root;
+
+      while(currentNode) {
+        if(newNode.data < currentNode.data) {
+          if(!currentNode.left) {
+            currentNode.left = newNode;
+            return;
+          }
+          currentNode = currentNode.left;
+         } else {
+          if(!currentNode.right) {
+            currentNode.right = newNode;
+            return;
+          }
+          currentNode = currentNode.right;
+         }
+      }
+  }
+}
+
+
+const tree = new BinarySearchTree();
+tree.insert(8);
+tree.insert(11);
+tree.insert(15);
+tree.insert(6);
+
+
+  // insert(data) {
+  //   let newNode = new Node(data);
+  //   if (this.root === null) {
+  //       this.root = newNode;
+  //   } else {
+  //       this.insertNode(this.root, newNode); // helper method below
+  //   }
+  // }
+
+  // insertNode(node, newNode) {
+  //   if (newNode.data < node.data) {
+  //       if (node.left === null) {
+  //           node.left = newNode;
+  //       } else {
+  //           this.insertNode(node.left, newNode);
+  //       }
+  //   } else {
+  //       if (node.right === null) {
+  //           node.right = newNode;
+  //       } else {
+  //           this.insertNode(node.right, newNode);
+  //       }
+  //   }
+  // }
+
+
