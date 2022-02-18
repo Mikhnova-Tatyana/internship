503eb3b38042847899ee77b28eeb42077ae0cb07
Tanya <tanjamikhnva@gmail.com>
2022-02-18 18:13:50 +0200

    regExp

--- fifth_module/regularExpressions.js
+++ fifth_module/regularExpressions.js
@@ -2,21 +2,21 @@ function checkIsPhoneNumber(phoneNumber) {
   if (typeof phoneNumber !== 'string') {
     throw new Error('Invalid argument');
   }
-  return /\+\d{2}\(\d{3}\)\d{3}(-\d{2}){2}/.test(phoneNumber)
+  return /\+\d{2}\(\d{3}\)\d{3}(-\d{2}){2}/.test(phoneNumber);
 }
 
 function checkIsEmail(email) {
   if (typeof email !== 'string') {
     throw new Error('Invalid argument');
   }
-  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(email)
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(email);
 }
 
 function checkIsSite(site) {
   if (typeof site !== 'string') {
     throw new Error('Invalid argument');
   }
-  return /^https?\:\/\/(\w+\.?)+$/.test(site)
+  return /^https?\:\/\/(\w+\.?)+$/.test(site);
 }
 
 function checkIsValidPassword (password) {
