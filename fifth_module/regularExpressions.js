5e8b61e1f547c97dd8f199c5f023fd2168853c2f
Tanya <tanjamikhnva@gmail.com>
2022-02-18 18:12:15 +0200

    regExp

--- fifth_module/regularExpressions.js
+++ fifth_module/regularExpressions.js
@@ -5,17 +5,12 @@ function checkIsPhoneNumber(phoneNumber) {
   return /\+\d{2}\(\d{3}\)\d{3}(-\d{2}){2}/.test(phoneNumber)
 }
 
-checkIsPhoneNumber('+28(090)496-77-90')
-
 function checkIsEmail(email) {
   if (typeof email !== 'string') {
     throw new Error('Invalid argument');
   }
   return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(email)
 }
-
-checkIsEmail('kapitoshka@gmail.com')
-
 
 function checkIsSite(site) {
   if (typeof site !== 'string') {
@@ -24,17 +19,12 @@ function checkIsSite(site) {
   return /^https?\:\/\/(\w+\.?)+$/.test(site)
 }
 
-checkIsSite('http://test.dev')
-
-
 function checkIsValidPassword (password) {
   if (typeof password !== 'string') {
     throw new Error('Invalid argument');
   }
   return /^[a-zA-Z0-9_]{6,25}$/.test(password);
 }
-
-checkIsValidPassword('tyYB_6')
 
 function checkIpv4IsValid(ipv4String) {
   if (typeof ipv4String !== 'string') {
@@ -43,6 +33,3 @@ function checkIpv4IsValid(ipv4String) {
   let pattern = /^(([01]?\d\d?|2[0-4]\d|25[0-5])\.){3}([01]?\d\d?|2[0-4]\d|25[0-5])$/;
   return pattern.test(ipv4String);
 }
-
-checkIpv4IsValid('127.255.255.255')
-checkIpv4IsValid('0.0.0.0')
